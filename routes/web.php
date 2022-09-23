69a8d46904b0406121aaa4710b8706f7c56722c0
hankusa1709 <handyk1709@gmail.com>
2022-09-24 01:52:52 +0700

    fixed not imported controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Portfolio\PortfolioController;
+use App\Http\Controllers\Index\IndexController;
 
 /*
 |--------------------------------------------------------------------------
